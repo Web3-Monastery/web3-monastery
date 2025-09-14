137642391e68d667ffc5408d34f7d4973da45e4d
BigMike <peter@flynn.network>
2025-09-14 16:16:04 -0700

    Change keys for peter

--- modules/users/peter.nix
+++ modules/users/peter.nix
@@ -4,8 +4,8 @@ let
   username = "peter";
   hashedPassword = "$y$j9T$9x/UcVeGMmD9dBINipPqR/$D9OAfBQ3lQ83h2FJ4qIsGNLc14zsPQsI/Q67vaM21U3";
   keys = [
-    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBIo4B7eTpgx1pB4/WjrDjjWbq07Yuq1OujiKl6EWPPLjXFmuCZ0ha7AvcJYARfG7ClOC1gXKXp8alZXooTVc5SU= PeterFlynn-OP7Pro"
-    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBC4eSw2cFIGWa5VIhnOrVDam46QRZVkwrEs19RllAcaxhBzZted0HfSs3AxcEQGIZPv00GDu4qypatTv5qZMuaw= MNNC-PeterFlynn-G16"
+    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBLn5IE0UXy/ChgVw0IRMsnkRtjmWEfOek51+aRrmwEDHwK70nw8ZN8EUvO5BdP130olimvvmkP8iH5A88qRx2nA= PeterFlynn-OP7Pro"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHLdOh3YaKS16zF7SgOLjZOif8YEgnx4A884JsFdZSgz peter@PeterFlynn-Server"
   ];
 in {
   users.users.${username} = {
