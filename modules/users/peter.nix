8662365ad1119bc3d9b62d66e6ec8baa280a47a6
Peter Flynn <dev@flynn.network>
2025-11-12 17:23:54 -0800

    Add key for peter

--- modules/users/peter.nix
+++ modules/users/peter.nix
@@ -6,6 +6,7 @@ let
   keys = [
     "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBLn5IE0UXy/ChgVw0IRMsnkRtjmWEfOek51+aRrmwEDHwK70nw8ZN8EUvO5BdP130olimvvmkP8iH5A88qRx2nA= PeterFlynn-OP7Pro"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHLdOh3YaKS16zF7SgOLjZOif8YEgnx4A884JsFdZSgz peter@PeterFlynn-Server"
+    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFPdiSj861D1jYQY7WrSHrrUBupP6J8KL7yfg2iKAo3wa/h6tEyBY5NHjP1kSS8S73Z5PjoHhPi3OM9RzA2lZZo= PeterFlynn-Dev"
   ];
 in {
   users.users.${username} = {
